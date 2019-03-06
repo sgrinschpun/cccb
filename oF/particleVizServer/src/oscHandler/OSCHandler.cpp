435e1a31c99ac48366b436b30b3f0df86c9ce14d
sgrinschpun <sgrinschpun@ifae.es>
2019-03-06 22:20:06 +0100

    attributes, log commented and keyboard launcher purge added

--- oF/particleVizServer/src/oscHandler/OSCHandler.cpp
+++ oF/particleVizServer/src/oscHandler/OSCHandler.cpp
@@ -11,27 +11,27 @@
 OSCHandler::OSCHandler(){}
 
 void OSCHandler::acumulativeParticleParse(ofxOscMessage message){
-    if(message.getAddress() == "/particle/attribute/id")
+    if(message.getAddress() == "/particle/attributes/id")
         middleParams.id = message.getArgAsInt(0);
-    if(message.getAddress() == "/particle/attribute/parent")
+    if(message.getAddress() == "/particle/attributes/parent")
         middleParams.parent = message.getArgAsInt(0);
-    if(message.getAddress() == "/particle/attribute/name")
+    if(message.getAddress() == "/particle/attributes/name")
         middleParams.name = message.getArgAsString(0);
-    if(message.getAddress() == "/particle/attribute/mass")
+    if(message.getAddress() == "/particle/attributes/mass")
         middleParams.mass = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/charge")
+    if(message.getAddress() == "/particle/attributes/charge")
         middleParams.charge = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/type")
+    if(message.getAddress() == "/particle/attributes/type")
         middleParams.type = message.getArgAsString(0);
-    if(message.getAddress() == "/particle/attribute/E")
+    if(message.getAddress() == "/particle/attributes/E")
         middleParams.energy = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/vx")
+    if(message.getAddress() == "/particle/attributes/vx")
         middleParams.vx = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/vy")
+    if(message.getAddress() == "/particle/attributes/vy")
         middleParams.vy = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/vz")
+    if(message.getAddress() == "/particle/attributes/vz")
         middleParams.vz = message.getArgAsFloat(0);
-    if(message.getAddress() == "/particle/attribute/beta")
+    if(message.getAddress() == "/particle/attributes/beta")
         middleParams.beta = message.getArgAsFloat(0);
 
     // if(message.getAddress() == "/particle/attribute/p")
