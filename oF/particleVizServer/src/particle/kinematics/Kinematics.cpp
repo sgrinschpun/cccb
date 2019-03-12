32c75378867bbcd186fcf46db806074060261d7b
sgrinschpun <sgrinschpun@ifae.es>
2019-03-12 23:12:29 +0100

    tyding up

--- oF/particleVizServer/src/particle/kinematics/Kinematics.cpp
+++ oF/particleVizServer/src/particle/kinematics/Kinematics.cpp
@@ -1,12 +1,7 @@
 #include "Kinematics.h"
 
 Kinematics::Kinematics(shared_ptr<ParticleData>& _particleData, ofVec2f _position, ofVec2f _velocity): data(_particleData), position(_position), velocity(_velocity){
-
-  //Bfield.set(0,0,0.0000015);
   acceleration.set(0,0);
-  //initSpeed = 30;
-  //finalSpeed = velocity.length();
-  //initTime = 50*ofGetElapsedTimeMillis();
   velocity *= 1.5;
 }
 
@@ -14,13 +9,11 @@ void Kinematics::setBforce(){
   float q = data->getCharge();
   float mass = data->getMass();
   ofVec2f perpendicular= velocity.getPerpendicular();
-  Bforce = 0.0000015*perpendicular*q/mass;
-  //velocity.getCrossed(Bfield)*q/mass;
+  Bforce = 0.00005*perpendicular*q/mass;
 }
 
 void Kinematics::setVelocity(){
   float elapsedTime = 50*ofGetElapsedTimeMillis() - initTime;
-  //float speed = initSpeed*exp(-pow(elapsedTime,2)/10) + finalSpeed;
   float speed = initSpeed/(1+pow(elapsedTime,2))+ finalSpeed;
   velocity.normalize();
   velocity *= speed;
@@ -33,7 +26,6 @@ ofVec2f Kinematics::applyForce(ofVec3f _vector){
 
 
 void Kinematics::update(){
-  //setVelocity();
   setBforce();
   applyForce(Bforce);
   velocity += acceleration;
