da1a1e3534ab79a08984bb61639bde25e37e455d
sgrinschpun <sgrinschpun@ifae.es>
2019-03-18 15:22:34 +0100

    starts with implementation of commons assets including fonts and parameters

---
+++ oF/particleVizServer/src/commons/Fonts.h
@@ -0,0 +1,10 @@
+#ifndef _Fonts //
+#define _Fonts //
+#include "ofMain.h"
+#include "ofxCenteredTrueTypeFont.h"
+
+class Fonts {
+  static ofxCenteredTrueTypeFont font;
+  void setupFont();
+};
+#endif
